c171509ae7db1d235f2d3b83fc6525987f48ffe1
joshuat-23 <124537595+joshuat-23@users.noreply.github.com>
2023-03-23 14:34:53 +0800

    Add files via upload

---
+++ Year-11-CS-Unit-7-Tutorial-ClassSection/src/ClassSection.java
@@ -0,0 +1,57 @@
+public class ClassSection {
+
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private Arraylist<Student> students;
+
+    public ClassSection(String subject, int capacity, int yearLevel){
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
+
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getYearLevel(){
+        return yearLevel;
+    }
+
+    public void setYearLevel(int yearLevel){
+        this.yearLevel = yearLevel;
+    }
+
+    public void getStudents(){
+
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void removeStudent(){
+
+    }
+
+    public void isStudentEnrolled(){
+        
+    }
+
+
+
+
+}
